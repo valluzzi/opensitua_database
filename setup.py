834426dfafd225d2dee367d52327bd1aaad423dd
Valerio Luzzi <valerio.luzzi@gecosistema.it>
2019-09-11 18:00:19 +0200

    commit of 11/09/2019 18:00:19.94

--- setup.py
+++ setup.py
@@ -31,5 +31,5 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ),
-    install_requires=[]
+    install_requires=['opensitua_core']
 )
